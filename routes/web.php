f9858a2e38cbf62a897a50915f4826b1894478c8
rezalastana <rezalastana21@gmail.com>
2024-05-21 22:10:38 +0700

    feat: settingUp googleFont-daisyTheme

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Livewire\Auth\Login;
+use App\Livewire\Auth\Profile;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,11 +12,11 @@ Route::get('/', function () {
 
 //group middleware
 Route::middleware('auth')->group(function () {
-    Route::get('/home')->name('home');
-    Route::get('/profile')->name('home');
+    Route::get('/home', Home::class)->name('home');
+    Route::get('/profile', Profile::class)->name('home');
 });
 
 //middleware guest, sehingga kalau belum login tidak bisa akses halaman pada middleware auth
 Route::middleware('guest')->group(function () {
-    Route::get('/login')->name('login');
+    Route::get('/login', Login::class)->name('login');
 });
