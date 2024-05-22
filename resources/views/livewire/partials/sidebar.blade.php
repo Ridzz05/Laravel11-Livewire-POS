1ad34cfd795442061b986b4cc2f7a71853f75935
rezalastana <rezalastana21@gmail.com>
2024-05-22 13:51:36 +0700

    feat: NavBar

--- resources/views/livewire/partials/sidebar.blade.php
+++ resources/views/livewire/partials/sidebar.blade.php
@@ -1,5 +1,60 @@
-<ul class="menu min-h-full w-80 bg-base-200 p-4 text-base-content">
-    <!-- Sidebar content here -->
-    <li><a>Sidebar Item 1</a></li>
-    <li><a>Sidebar Item 2</a></li>
+<ul class="menu min-h-full w-80 space-y-4 border-r-2 border-base-300 bg-base-100 p-4 text-base-content">
+    <li>
+        <h2 class="menu-title">Dashboard</h2>
+        <ul>
+            <li>
+                {{-- akan aktif jika route sesuai dengan yang ada di href --}}
+                <a href="{{ route('home') }}" @class(['active' => Route::is('home')]) wire:navigate>
+                    <x-tabler-dashboard class="size-5" />
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="" @class(['active' => false]) wire:navigate>
+                    <x-tabler-file-plus class="size-5" />
+                    <span>Input Transaksi</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <h2 class="menu-title">Data Master</h2>
+        <ul>
+            <li>
+                <a href="" @class(['active' => false]) wire:navigate>
+                    <x-tabler-layout-grid-add class="size-5" />
+                    <span>Menu Makanan</span>
+                </a>
+            </li>
+            <li>
+                <a href="" @class(['active' => false]) wire:navigate>
+                    <x-tabler-users class="size-5" />
+                    <span>Data Customer</span>
+                </a>
+            </li>
+            <li>
+                <a href="" @class(['active' => false]) wire:navigate>
+                    <x-tabler-file class="size-5" />
+                    <span>Riwayat Transaksi</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <h2 class="menu-title">Account</h2>
+        <ul>
+            <li>
+                <a href="{{ route('profile') }}" @class(['active' => Route::is('profile')]) wire:navigate>
+                    <x-tabler-user class="size-5" />
+                    <span>Edit Profile</span>
+                </a>
+            </li>
+            <li>
+                <a href="" @class(['active' => false]) wire:navigate>
+                    <x-tabler-logout class="size-5" />
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+    </li>
 </ul>
