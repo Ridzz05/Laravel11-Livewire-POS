3c80578ae53bbf44330f07a4c5cbd6748572898f
rezalastana <rezalastana21@gmail.com>
2024-06-10 13:20:58 +0700

    feat: detailTransaction-from-menuPilihan

--- app/Livewire/Transaksi/Actions.php
+++ app/Livewire/Transaksi/Actions.php
@@ -2,14 +2,73 @@
 
 namespace App\Livewire\Transaksi;
 
+use App\Livewire\Forms\TransaksiForm;
 use App\Models\Customer;
 use App\Models\Menu;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class Actions extends Component
 {
-    //search attribute
+    //search public property
     public $search;
+
+    //item total public property
+    public $items = []; //ini akan menyimpan 2 data obj yaitu qty dan price
+
+    public ?TransaksiForm $form;
+
+    public function addItem(Menu $menu)
+    {
+        //$menu->name adalah key
+        if (isset($this->items[$menu->name])) {
+            // jika item sudah ada, tambahkan qty
+            $item = $this->items[$menu->name];
+
+            //tambahkan qty dan price jika item sudah ada
+            $this->items[$menu->name] = [
+                'qty' => $item['qty'] + 1,
+                'price' => $item['price'] + $menu->price,
+            ];
+        } else {
+            $this->items[$menu->name] = [
+                'qty' => 1,
+                'price' => $menu->price,
+            ];
+        }
+        // dd($this->items);
+    }
+
+    public function removeItem($key)
+    {
+        $item = $this->items[$key];
+
+        //jika qty lebih dari 1, kurangi qty dan price
+        if ($item['qty'] > 1) {
+            //deklare
+            $hargaSatuan = $item['price'] / $item['qty'];
+            $qtyBaru = $item['qty'] - 1;
+
+            //kurangi qty dan price jika item lebih dari 1
+            $this->items[$key]['qty'] = $qtyBaru;
+            $this->items[$key]['price'] = $hargaSatuan * $qtyBaru;
+        } else {
+            //unset
+            unset($this->items[$key]);
+        }
+    }
+
+    public function getTotalPrice()
+    {
+        $prices = array_column($this->items, 'price');
+
+        //return sum
+        return array_sum($prices);
+    }
+
+    public function simpan()
+    {
+    }
 
     public function render()
     {
--- resources/views/livewire/transaksi/actions.blade.php
+++ resources/views/livewire/transaksi/actions.blade.php
@@ -16,11 +16,12 @@
                         @foreach ($menu as $item)
                             {{-- class avatar milik daisyui --}}
                             <div class="flex flex-col">
-                                <div class="avatar">
+                                {{-- ubah menjadi button karena jika di klik akan mengarah ke method addItem yang berada pada Action Transaksi dan ditambahkan pada table disamping --}}
+                                <button class="avatar" wire:click='addItem({{ $item->id }})'>
                                     <div class="w-full rounded">
                                         <img src="{{ $item->gambar }}" alt="">
                                     </div>
-                                </div>
+                                </button>
                                 <h5 class="text-center text-[10px] font-normal">{{ $item->name }}</h5>
                             </div>
                         @endforeach
@@ -30,21 +31,34 @@
         </div>
         {{-- sebelah kanan --}}
         <div class="card h-fit">
-            <div class="card-body space-y-4">
+            <form class="card-body space-y-4" wire:submit='simpan'>
                 <h3 class="card-title">Detail transaksi</h3>
 
+                {{-- @json($items, JSON_PRETTY_PRINT) --}}
                 {{-- table --}}
                 <div class="table-wrapper">
                     <table class="table">
                         <thead>
-                            <th>No</th>
                             <th>Nama Menu</th>
                             <th>Harga</th>
                             <th>QTY</th>
                             <th></th>
                         </thead>
                         <tbody>
-
+                            {{-- <span>{{ dd($items['qty']) }}</span> --}}
+                            @foreach ($items as $key => $value)
+                                <tr>
+                                    <td>{{ $key }}</td>
+                                    <td>{{ $value['qty'] }}</td>
+                                    <td>{{ Number::format($value['price']) }}</td>
+                                    <td>
+                                        <button class="btn btn-square btn-xs"
+                                            wire:click="removeItem('{{ $key }}')">
+                                            <x-tabler-minus class="size-4" />
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -62,14 +76,14 @@
                 <div class="card-actions justify-between">
                     <div class="flex flex-col">
                         <div class="text-xs">Total</div>
-                        <div class="card-title">Rp. 0</div>
+                        <div class="card-title">Rp. {{ Number::format($this->getTotalPrice()) }}</div>
                     </div>
                     <button class="btn btn-primary">
                         <x-tabler-check class="size-5" />
                         <span>Simpan</span>
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
