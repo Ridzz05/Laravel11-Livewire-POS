dc9ea5fdbaf5c4f75f329649ef7a9b9e0be1a9d7
rezalastana <rezalastana21@gmail.com>
2024-06-20 11:50:19 +0700

    feat: Home-Grid

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Transaksi;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.home');
+        // ambil tahun dan bulan dengan menggunakan explode dari Y-m
+        [$tahun, $bulan] = explode('-', date('Y-m'));
+        // atau bisa satuan
+        $today = date('Y-m-d');
+
+        $transaksi = Transaksi::whereMonth('created_at', $bulan)->whereYear('created_at', $tahun);
+        return view('livewire.home', [
+            'monthly' => $transaksi->get()->sum('price'),
+            'today' => $transaksi->whereDate('created_at', $today)->get(),
+        ]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,9 +1,47 @@
-<div>
-    Hello world
-    <thead>
-        <td>
-            <h1>Home</h1>
-
-        </td>
-    </thead>
+<div class="page-wrapper">
+    {{-- bagi menjadi 3 kolom --}}
+    <div class="grid grid-cols-3 gap-6">
+        {{-- card 1 --}}
+        <div class="card card-compact">
+            <div class="card-body flex-row items-center gap-4">
+                <div class="avatar placeholder">
+                    <div class="w-12 rounded-full bg-warning">
+                        <x-tabler-calendar-month class="size-6" />
+                    </div>
+                </div>
+                <div class="flex flex-col">
+                    <div class="text-xs opacity-60">Pendapatan bulan ini</div>
+                    <div class="text-2xl font-bold">Rp. {{ Number::format($monthly) }}</div>
+                </div>
+            </div>
+        </div>
+        {{-- card 2 --}}
+        <div class="card card-compact">
+            <div class="card-body flex-row items-center gap-4">
+                <div class="avatar placeholder">
+                    <div class="w-12 rounded-full bg-warning">
+                        <x-tabler-calendar-check class="size-6" />
+                    </div>
+                </div>
+                <div class="flex flex-col">
+                    <div class="text-xs opacity-60">Pendapatan hari ini</div>
+                    <div class="text-2xl font-bold">Rp. {{ Number::format($today->sum('price')) }}</div>
+                </div>
+            </div>
+        </div>
+        {{-- card 3 --}}
+        <div class="card card-compact">
+            <div class="card-body flex-row items-center gap-4">
+                <div class="avatar placeholder">
+                    <div class="w-12 rounded-full bg-warning">
+                        <x-tabler-list-check class="size-6" />
+                    </div>
+                </div>
+                <div class="flex flex-col">
+                    <div class="text-xs opacity-60">Pesanan hari ini</div>
+                    <div class="text-2xl font-bold">{{ $today->count() }} Pesanan</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
